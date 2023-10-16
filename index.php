6cb2b86bd31ef54e594784642b2969be19eb134d
Adrian YANKOU <adroen.yankou@groupe-esigelec.org>
2023-10-16 15:02:09 +0200

    création de la page d'accueil

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>les pommes pilees</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Exercice Bootstrap</h1>
+    <div class="container">
+        <div class="row">
+           <p class="col-lg-3">Démarrez uWamp1
+            (c:\logiciel\uWamp dans les salles de TP)
+           2. Créez un dossier « exemple », dans les dossier www,
+           3. Ouvrez ce dossier avec Visual Studio Code :
+           4. Créez une fichier index.html,
+           5. Copiez le code d’une page contenant les liens CSS et JS de Bootstrap,
+           6. Changez le titre et testez le rendu (url : localhost/exemple),
+           7. Créez une div container, et à l’intérieur une div row,
+           8. Ajoutez deux paragraphes (lorem),
+           9. Ainsi qu’une div contenant une image de votre choix,
+           </p> 
+           <p class="col-lg-3">Appliquez les classes Bootstrap permettant d’avoir le rendu des Figures 1 et 2 en fonction de
+            la taille de l’image (breakpoint : Large). Testez,
+            11. Pour que l’image s’adapte à la taille de la page, appliquez-lui la classe img-fluid,
+            12. Créez un fichier style.css dans le dossier exemple, insérer un lien vers ce fichier dans votre
+            page web, sous le lien vers le fichier css de Bootstrap,
+            13. Dans style.css ajoutez une règle pour les paragraphes, avec la propriété :
+            text-align: justify;
+           </p> 
+        </div class="col-lg-6">
+            <img src="Before School dump.png"  alt="">
+           </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
