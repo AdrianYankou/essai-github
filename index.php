0cc96c8429d05080831d18d28cc38c9904dccf99
Adrian YANKOU <adroen.yankou@groupe-esigelec.org>
2023-10-16 17:11:26 +0200

    modif

--- index.php
+++ index.php
@@ -8,5 +8,17 @@
 </head>
 <body>
 <img src="sw.png" alt="Entrée de l'Esigelec">
+
+<div class="container">
+        <div class="row">
+           <p class="col-lg-3">Démarrez uWamp1
+           s'identifier
+           </p> 
+           <p class="col-lg-3">Appliquez les classes Bootstrap permettant d’avoir le rendu des Figures 1 et 2 en fonction de
+            creer un compte
+           </p> 
+        
+       </div>
+   </div>
 </body>
 </html>
