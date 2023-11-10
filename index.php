5daf6be299d19c6f97c94fa70674e4ca5eacf8da
Adrian YANKOU <adroen.yankou@groupe-esigelec.org>
2023-11-10 14:10:58 +0100

    44

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
       <img src="sw.png" width="100%" heigth="100%" 
       object-fit: cover; >
    </div>
-   <div style="position:absolute;top:360px; width:600px; height:400px; z-index:2;font-size:200%">
+   <div style="position:absolute;top:360px; width:100%px; height:100%px; z-index:2;font-size:200%">
       <center><b>Bienvenu dans l'univers du jeu</b></center>
     </div> 
 </body>
