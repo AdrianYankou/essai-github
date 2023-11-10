abb9174d3f683907b75cf95dc55669c763974f90
tanouchka <148068161+tanouchka@users.noreply.github.com>
2023-11-10 23:30:32 +0100

    rr

--- index.php
+++ index.php
@@ -4,12 +4,26 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <style>
-   b {
-      color: white;
-   }
-  </style>
+  
   <title>premiere page</title>
+  <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+
+        header {
+            text-align: center;
+            padding: 10px;
+            background-color: #f2f2f2;
+        }
+
+        main {
+            margin-top: 20px;
+        }
+
+        
+    </style>
 </head>
 <body>
 
@@ -19,8 +33,9 @@
    </div>
    <div style="position:absolute;top:360px; width:600px; height:400px; z-index:2;font-size:200%">
    <p class="texte-bleu"></p>
-   <center><a href="deuxiemepage.html"><button class="c" type="button" style="display: block; margin: 150px auto 0; font-size: 15px; font-weight: lighter; color: white; padding: 25px 80px; background: #000000; outline: none; cursor:pointer; border:none; border-radius: 20px; position: relative; box-shadow: 0 6px #000000; align-item:">s'authentifier</button></a>
-      <a href="page3.html"><button class="c" type="button" style="display: block; margin: 150px auto 0; font-size: 15px; font-weight: lighter; color: white; padding: 25px 80px; background: #000000; outline: none; cursor:pointer; border:none; border-radius: 20px; position: relative; box-shadow: 0 6px #000000; align-item:">s'inscrire</button></a></center>
+   <center><a href="connexion.html"><button class="c" type="button" style="display: block; margin: 150px auto 0; font-size: 15px; font-weight: lighter; color: white; padding: 25px 80px; background: #000000; outline: none; cursor:pointer; border:none; border-radius: 20px; position: relative; box-shadow: 0 6px #000000; align-item:">s'authentifier</button></a>
+      <br>
+      <a href="authentification .html"><button class="c" type="button" style="display: block; margin: 150px auto 0; font-size: 15px; font-weight: lighter; color: white; padding: 25px 80px; background: #000000; outline: none; cursor:pointer; border:none; border-radius: 20px; position: relative; box-shadow: 0 6px #000000; align-item:">s'inscrire</button></a></center>
    
    </p>
    
