c6b9ceefb94787e335631caad77a3d2f471e99d7
tanouchka <148068161+tanouchka@users.noreply.github.com>
2023-11-11 08:38:48 +0100

    ujh

---
+++ essaie.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>page d'authentification</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+
+        header {
+            text-align: center;
+            padding: 10px;
+            background-color: #f2f2f2;
+        }
+
+        main {
+            margin-top: 20px;
+            text-align: center;
+        } 
+        button{    
+         display: block;
+         margin: 150px auto 0;
+         font-size: 15px; 
+         font-weight: lighter;
+         color: white;
+         padding: 25px 80px;
+         background: #000000;
+         outline: none;
+         cursor:pointer;
+         border:none;
+         border-radius: 20px; 
+         position: relative;
+         box-shadow: 0 6px #000000;
+       
+      }
+
+    
+    </style>
+</head>
+<body>
+
+    
+    
+    <main>
+        <div style="position:absolute;z-index:1">
+            <img src="sw.png"  width="100%" heigth="100%" 
+            object-fit:cover; >
+         </div>
+       
+
+       
+        <a href="connexion.html"><button>s'authentifier</button></a>
+        <a href="inscription.html"><button>s'inscrire</button></a>
+    </main>
+</body>
+</html>
+<body>
+    
+    
+    
+
+
+
+
+
+
+
+
