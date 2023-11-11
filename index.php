94241a885ffc2720cb9ed7512ece5f6afaae28df
AdrianYankou <adroen.yankou@groupe-esigelec.org>
2023-11-11 21:59:19 +0100

    ajout fond d'ecran partout

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
          font-family: Arial, sans-serif;
          margin: 0;
          padding: 0;
-         background-image: url('sw.png');
+         background-image: url('background Accueil.png');
          background-size: cover;
          background-position: center;
          background-repeat: no-repeat;
