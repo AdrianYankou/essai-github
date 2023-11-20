3cc03ce55e1e115ac02ab6e2eec081d480a51d5a
tanouchka <148068161+tanouchka@users.noreply.github.com>
2023-11-20 17:24:32 +0100

    kjh

--- jsp.php
+++ jsp.php
@@ -188,3 +188,135 @@ if ($stmt = $mysqli->prepare("SELECT motdepasse, statut FROM utilisateur WHERE m
     redirectTo('connexion.html', 'Erreur lors de l\'authentification.');
 }
 ?>
+
+
+
+
+
+
+
+<?php
+
+ 
+
+  session_start(); // Pour les massages
+
+ 
+
+ 
+
+  $mail =  htmlentities($_POST['email']);
+
+  $password = htmlentities($_POST['password']);
+
+ 
+
+  // Connexion :
+
+  require_once("param.inc.php");
+
+  $mysqli = new mysqli($host, $login, $password, $dbname);
+
+  if ($mysqli->connect_error) {
+
+      die('Erreur de connexion (' . $mysqli->connect_errno . ') '
+
+              . $mysqli->connect_error);
+
+  }
+
+ 
+
+ 
+
+ 
+
+ 
+
+  if ($stmt = $mysqli->prepare("SELECT * FROM users WHERE email=? limit 1"))
+
+  {
+
+   
+
+    $stmt->bind_param("s", $email);
+
+    $stmt->execute();
+
+    $result = $stmt->get_result();  
+
+ 
+
+    if($result->num_rows > 0)
+
+    {    
+
+     
+
+        $row = $result->fetch_assoc();
+
+            if (password_verify($password,$row["password"]))
+
+            {
+
+              echo "connexion reussi";
+
+                  // Redirection vers la page admin.php ou autres pages en fonction du role (tuteur,admin, etc.);
+
+                  $_SESSION['PROFILE']=$row;
+
+                //$_SESSION['message'] = "Authentification réussi pour un role inconnu.";
+
+                if($row["role"]==admin){
+
+                 
+
+                  $_SESSION['message'] = "Authentification réussi pour un admin.";
+
+                 
+
+                   header('Location: admin.php');
+
+                }
+
+                if($row["role"]==membre)
+
+                {
+
+                $_SESSION['message'] = "Authentification réussi pour un membre.";
+
+                 header('Location: sessionmembre.php');
+
+              }          
+
+           
+
+              }else {
+
+                // Redirection vers la page d'authetification connexion.php
+
+              $_SESSION['message'] = "Erreur de connexion";
+
+                 header('Location: connexion.html');
+
+               
+
+              }    
+
+       
+
+    }else{
+
+       
+
+      $_SESSION['message'] = "Erreur de connexion";
+
+         header('Location: conne.php');
+
+        }
+
+    }
+
+ 
+
+?>
